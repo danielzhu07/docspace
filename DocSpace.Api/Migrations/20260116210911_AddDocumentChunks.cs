fe2d9be5e446b24fc2d24a0b83497b0cd85c6599
Daniel Zhu <daniel_zhu25@outlook.com>
2026-01-16 13:19:53 -0800

    Added DocumentChunks table

---
+++ DocSpace.Api/Migrations/20260116210911_AddDocumentChunks.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DocSpace.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDocumentChunks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float[]>(
+                name: "Embedding",
+                table: "Documents",
+                type: "real[]",
+                nullable: false,
+                defaultValue: new float[0],
+                oldClrType: typeof(float[]),
+                oldType: "real[]",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "DocumentChunks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    DocumentId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ChunkIndex = table.Column<int>(type: "integer", nullable: false),
+                    Content = table.Column<string>(type: "text", nullable: false),
+                    Embedding = table.Column<float[]>(type: "real[]", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DocumentChunks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DocumentChunks_Documents_DocumentId",
+                        column: x => x.DocumentId,
+                        principalTable: "Documents",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DocumentChunks_DocumentId",
+                table: "DocumentChunks",
+                column: "DocumentId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DocumentChunks");
+
+            migrationBuilder.AlterColumn<float[]>(
+                name: "Embedding",
+                table: "Documents",
+                type: "real[]",
+                nullable: true,
+                oldClrType: typeof(float[]),
+                oldType: "real[]");
+        }
+    }
+}
